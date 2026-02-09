0a1524412399b9ff7b4522f574cd622e70877264
Арсений Шмелев <ariushmelev@edu.hse.ru>
2026-02-09 17:23:21 +0300

    Update with BERT

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def load_user_features():
     На этом этапе читаем таблицу с колонокой user_id из БД с фичами пользователей
     """
 
-    TABLE_NAME = "arsenij_shmelev_gwh6987_features_lesson_22"  
+    TABLE_NAME = "arsenij_shmelev_gwh6987_features_lesson_22_user"  
     query = f"SELECT * FROM {TABLE_NAME}"
 
     user_features = batch_load_sql(query)
@@ -88,80 +88,23 @@ def load_post_features():
     На этом этапе читаем таблицу с колонокой user_id из БД с фичами пользователей
     """
 
-    TABLE_NAME = "arsenij_shmelev_gwh6987_features_lesson_22"
+    TABLE_NAME = "arsenij_shmelev_gwh6987_features_lesson_22_post"
 
     engine = get_engine()
 
-    post_text_df = pd.read_sql(
-        "SELECT * FROM public.post_text_df",
+    post_features = pd.read_sql(
+        f"SELECT * FROM public.{TABLE_NAME}",
         con=engine
     )
 
-    posts_df = post_text_df[["post_id", "text", "topic"]].copy()
-    posts_df = posts_df.rename(columns={"post_id": "id"})
-    posts_df = posts_df.set_index("id")
-
-    ohe_topic = pd.get_dummies(
-        post_text_df["topic"],
-        dummy_na=False,
-        dtype=int
-    )
-
-    post_features = pd.concat(
-        [post_text_df[["post_id"]], ohe_topic],
-        axis=1
-    )
-
-    text_series = post_text_df["text"].fillna("")
-    post_features["text_len"] = text_series.str.split().apply(len)
-    post_features["unique_word_count"] = text_series.apply(
-        lambda x: len(set(x.split()))
-    )
-
-    post_features["text_diversity"] = post_features.apply(
-        lambda x: x["unique_word_count"] / x["text_len"] if x["text_len"] > 0 else 0,
-        axis=1
-    )
-
-    N = 1000000
-
-    feed_query = f"""
-    SELECT "timestamp", user_id, post_id, target
-    FROM public.feed_data
-    WHERE action = 'view'
-    ORDER BY "timestamp" DESC
-    LIMIT {N}
-    """
-
-    feed_data = pd.read_sql(feed_query, con=engine, parse_dates=["timestamp"])
-
-    post_stats = (
-        feed_data.groupby("post_id")
-        .agg(post_views=("target", "count"), post_likes=("target", "sum"))
-        .reset_index()
-    )
-
-    post_stats["post_ctr"] = (
-        post_stats["post_likes"] / post_stats["post_views"]
-    )
-
-    post_stats = post_stats.fillna(0)
-
-
-    post_features = post_features.merge(
-        post_stats,
-        on="post_id",
-        how="left"
-    )
-
-    for col in ["post_views", "post_likes", "post_ctr"]:
-        post_features[col] = post_features[col].fillna(0)
-
-    post_features = post_features.drop(columns=["unique_word_count"])
-
     post_features = post_features.set_index("post_id")
 
+    post_text_df = pd.read_sql(f"SELECT post_id, text, topic FROM public.post_text_df", con=engine)
+
+    posts_df = post_text_df.rename(columns={"post_id": "id"}).set_index("id")
+
     return post_features, posts_df
+    
 
 
 
@@ -173,7 +116,20 @@ user_cols = list(user_features.columns)
 post_cols = list(post_features.columns)
 time_cols = ["hour", "weekday", "is_weekend"]
 
-FEATURE_COLUMNS = time_cols + user_cols + post_cols
+try:
+    FEATURE_COLUMNS = list(model.feature_names_)
+    if not FEATURE_COLUMNS:
+        raise ValueError("Empty feature_names_")
+except Exception:
+    
+    time_cols = ["hour", "weekday", "is_weekend"]
+    user_cols = list(user_features.columns)
+    post_cols = list(post_features.columns)
+
+    post_cols = [c for c in post_cols if c != "unique_word_count"]
+    FEATURE_COLUMNS = time_cols + user_cols + post_cols
+
+
 
 
 def make_features_user(user_id: int, time: datetime) -> pd.DataFrame:
